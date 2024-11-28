c2734bbd45bc762abbd36ade8a88ae99ca40707b
harshitt13 <hk9084760388@gmail.com>
2024-11-28 11:48:52 +0530

    updated commit

--- src/lstm_model.py
+++ src/lstm_model.py
@@ -1,7 +1,84 @@
-import numpy as np
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import MinMaxScaler
-from keras.models import Sequential
-from keras.layers import LSTM, Dense, Dropout
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import LSTM, Dense
 
 import matplotlib.pyplot as plt
+
+# Load the dataset
+data_path = 'data/stock_data.csv'
+df = pd.read_csv(data_path)
+
+# Preprocess the data
+df['Date'] = pd.to_datetime(df['Date'])
+df.set_index('Date', inplace=True)
+data = df['Close'].values
+data = data.reshape(-1, 1)
+
+# Normalize the data
+scaler = MinMaxScaler(feature_range=(0, 1))
+scaled_data = scaler.fit_transform(data)
+
+# Create the training and test datasets
+train_size = int(len(scaled_data) * 0.8)
+train_data = scaled_data[:train_size]
+test_data = scaled_data[train_size:]
+
+# Create the dataset with look_back
+def create_dataset(dataset, look_back=1):
+    X, Y = [], []
+    for i in range(len(dataset) - look_back - 1):
+        a = dataset[i:(i + look_back), 0]
+        X.append(a)
+        Y.append(dataset[i + look_back, 0])
+    return np.array(X), np.array(Y)
+
+look_back = 60
+X_train, y_train = create_dataset(train_data, look_back)
+X_test, y_test = create_dataset(test_data, look_back)
+
+# Reshape input to be [samples, time steps, features]
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 1))
+X_test = np.reshape(X_test, (X_test.shape[0], X_test.shape[1], 1))
+
+# Build the LSTM model
+model = Sequential()
+model.add(LSTM(50, return_sequences=True, input_shape=(look_back, 1)))
+model.add(LSTM(50, return_sequences=False))
+model.add(Dense(25))
+model.add(Dense(1))
+
+# Compile the model
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+# Train the model
+model.fit(X_train, y_train, batch_size=1, epochs=1)
+
+# Make predictions
+train_predict = model.predict(X_train)
+test_predict = model.predict(X_test)
+
+# Invert predictions
+train_predict = scaler.inverse_transform(train_predict)
+y_train = scaler.inverse_transform([y_train])
+test_predict = scaler.inverse_transform(test_predict)
+y_test = scaler.inverse_transform([y_test])
+
+# Plot the results
+train_plot = np.empty_like(scaled_data)
+train_plot[:, :] = np.nan
+train_plot[look_back:len(train_predict) + look_back, :] = train_predict
+
+test_plot = np.empty_like(scaled_data)
+test_plot[:, :] = np.nan
+test_plot[len(train_predict) + (look_back * 2) + 1:len(scaled_data) - 1, :] = test_predict
+
+plt.figure(figsize=(16, 8))
+plt.plot(scaler.inverse_transform(scaled_data), label='Actual Stock Price')
+plt.plot(train_plot, label='Train Prediction')
+plt.plot(test_plot, label='Test Prediction')
+plt.xlabel('Date')
+plt.ylabel('Stock Price')
+plt.legend()
+plt.show()
